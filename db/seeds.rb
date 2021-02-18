8d625a629ec2633f4622cd39b02944667b902f9b
Eliza Plowden <elizaplowden@gmail.com>
2021-02-18 14:29:43 +0000

    validations, associations and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require 'open-uri'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+drink_serialized = open(url).read
+drink = JSON.parse(drink_serialized)
+
+puts "#{drink['name']}"
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
