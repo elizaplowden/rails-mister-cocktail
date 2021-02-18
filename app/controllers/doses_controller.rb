4df42cf20060f5daf9250f4f96d226ac40157954
Eliza Plowden <elizaplowden@gmail.com>
2021-02-18 16:43:22 +0000

    dose actions and views implemented

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,30 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(strong_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    @cocktail = @dose.cocktail
+    redirect_to cocktail_path(@cocktail.id)
+  end
+
+  private
+
+  def strong_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 require "json"
 require 'open-uri'
+
+Dose.destroy_all
+Ingredient.destroy_all
 
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 drink_serialized = open(url).read
 drink = JSON.parse(drink_serialized)
+list = drink["drinks"]
+list.each do |drink|
+  name = drink["strIngredient1"]
+  Ingredient.create!(name: name)
+end
 
-puts "#{drink['name']}"
+
 
 # Ingredient.create!(name: "lemon")
 # Ingredient.create!(name: "ice")
 # Ingredient.create!(name: "mint leaves")
 
-Cocktail.create!(name: "mohito")
+# Cocktail.create!(name: "mohito")
+# Cocktail.create!(name: "margarita")
