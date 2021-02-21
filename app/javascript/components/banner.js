66b847d18e11f3b2d6abb94997fd2af973258d9f
Eliza Plowden <elizaplowden@gmail.com>
2021-02-21 11:55:01 +0000

    added footer and links

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["A drink for every occasion"],
-    typeSpeed: 50,
+    typeSpeed: 75,
     loop: true
   });
 }
