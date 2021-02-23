d2a050a0676da7b9e4d99ff1c22bc3fc57aa9096
Eliza Plowden <elizaplowden@gmail.com>
2021-02-23 09:15:52 +0000

    typed js fix

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,6 +1,6 @@
 import Typed from 'typed.js';
 
-if (#banner-typed-text){
+// if (#banner-typed-text){
   const loadDynamicBannerText = () => {
     new Typed('#banner-typed-text', {
       strings: ["A drink for every occasion"],
@@ -8,6 +8,6 @@ if (#banner-typed-text){
       loop: true
     });
   }
-}
+// }
 
 export { loadDynamicBannerText };
